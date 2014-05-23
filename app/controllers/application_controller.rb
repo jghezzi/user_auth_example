22d4b2a630eb726b9e21be2a98c4840dc0623157
Jason Ghezzi <jasonghezzi@gmail.com>
2014-05-22 21:29:20 -0400

    refactored and moved api calls to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,24 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
+  def get_user_avatar(etsy_user_id)
+    etsy_user_avatar_hash = HTTParty.get("https://openapi.etsy.com/v2/users/#{etsy_user_id}/avatar/src?api_key=#{Rails.application.secrets.etsy_api_key}")
+    @etsy_user_avatar = etsy_user_avatar_hash["results"]["src"]
+  end
+
+  def get_user_details(etsy_user_id)
+    etsy_user_hash = HTTParty.get("https://openapi.etsy.com/v2/users/#{etsy_user_id}/profile?api_key=#{Rails.application.secrets.etsy_api_key}")
+    user_hash_header = etsy_user_hash["results"][0]
+    @etsy_shop_name = user_hash_header["login_name"]
+    @etsy_seller_first_name = user_hash_header["first_name"]
+    @etsy_seller_last_name = user_hash_header["last_name"]
+  end
+
+  def get_item_images(etsy_id)
+    response = HTTParty.get("https://openapi.etsy.com/v2/listings/#{etsy_id}/images?api_key=#{Rails.application.secrets.etsy_api_key}")
+    @images = response["results"]
+  end
+
   def ensure_logged_in
   	if current_user.nil?
   		redirect_to login_path
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,18 +2,9 @@ class ItemsController < ApplicationController
 
 	def show
 		@item = Item.find(params[:id])
-		
-		response = HTTParty.get("https://openapi.etsy.com/v2/listings/#{@item.etsy_id}/images?api_key=#{Rails.application.secrets.etsy_api_key}")
-		@images = response["results"]
-		
-		etsy_user_avatar_hash = HTTParty.get("https://openapi.etsy.com/v2/users/#{@item.etsy_user_id}/avatar/src?api_key=#{Rails.application.secrets.etsy_api_key}")
-		@etsy_user_avatar = etsy_user_avatar_hash["results"]["src"]
-
-		etsy_user_hash = HTTParty.get("https://openapi.etsy.com/v2/users/#{@item.etsy_user_id}/profile?api_key=#{Rails.application.secrets.etsy_api_key}")
-		@etsy_shop_name = etsy_user_hash["results"][0]["login_name"]
-		@etsy_seller_first_name = etsy_user_hash["results"][0]["first_name"]
-		@etsy_seller_last_name = etsy_user_hash["results"][0]["last_name"]
-
+		get_item_images(@item.etsy_id)
+		get_user_avatar(@item.etsy_user_id)
+		get_user_details(@item.etsy_user_id)
 	end
 
 	def new
